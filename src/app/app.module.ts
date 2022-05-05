c29ddeee51bd4f7c282eaab6f61a744fb9b7fa35
abdelsalam <abdelsalam.abakar@yahoo.fr>
2022-05-05 15:52:48 +0200

    Add components launch-detail and launch-programs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchDetailComponent } from './launch-detail/alunch-detail.component';
+import { LunchProgramsComponent } from './launch-programs/launch-programs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchDetailComponent,LunchProgramsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
