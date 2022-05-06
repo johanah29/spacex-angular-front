7868553d12ebe7201e1c1c9cded5dd7bccc623b3
johanah29 <jlekeutsane@yahoo.fr>
2022-05-06 15:44:23 +0200

    Adding sample home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { CapsuleComponent } from './components/capsule/capsule.component';
-import { LaunchDetailComponent } from './components/launch-detail/alunch-detail.component';
 import { LunchProgramsComponent } from './components/launch-programs/launch-programs.component';
 import { ListCapsuleComponent } from './components/list-capsule/list-capsule.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: ListCapsuleComponent },
+  { path: '', pathMatch: 'full', component: HomePageComponent },
   { path: 'capsules', component: ListCapsuleComponent},
   { path: 'capsule/:id', component: CapsuleComponent},
-  { path: 'lauches', component: LunchProgramsComponent},
+  { path: 'launches', component: LunchProgramsComponent},
   { path: '**', redirectTo: ''}
 ];
 
